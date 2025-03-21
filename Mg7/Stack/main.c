71bff4a38d9420beab429a12e3cd81c590ebd3aa
ElLukman <ellukmanahmad67@gmail.com>
2025-03-21 11:15:53 +0700

    final

--- Mg7/Stack/main.c
+++ Mg7/Stack/main.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+#include "linked.h"
 
-int main ()
+int main()
 {
-    printf("PROGRAM KONVERSI DESIMAL KE BINER MENGGUNAKAN STACK");
+    system("cls");
+    address top = NULL;
+    int desimal, hasil;  
+
+    // Input angka desimal
+    printf("Masukkan angka desimal: ");
+    scanf("%d", &desimal);
+
+    // Konversi desimal ke biner
+    DesimalConverterToBinary(desimal, &top);
     
+    // Print hasil stack setelah konversi
+    printf("\nHasil konversi ke biner (LIFO): \n");
+    PrintStack(top);
 
-
-
-
+    // Mengeluarkan angka biner satu per satu (stack LIFO)
+    printf("\nHasil konversi biner (dibaca dari bawah ke atas): ");
+    while (!isEmptyStack(top))
+    {
+        PopStack(&top, &hasil);
+        printf("%d", hasil);
+    }
+    printf("\n");
 
     return 0;
 }
--- Mg7/Stack/stack.h
+++ Mg7/Stack/stack.h
@@ -3,8 +3,10 @@
 #include "linked.h"
 
 //Method
-void PushStack(address *top, int nilai);
+void PushStack(address *top, boolean nilai);
 void PopStack(address *top, int *X);
-void DesimalConverter (int de);
+boolean isEmptyStack(address top);
+void DesimalConverterToBinary (int desimal, address *top);
+void PrintStack(address top);
 
 #endif
